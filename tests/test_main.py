9d5487b9c1845e3815a66bb94906bf50d6b79f5c
axiangcoding <axiangcoding@gmail.com>
2024-01-28 11:59:23 +0800

    code: raise error when api return error message (#36)
    - **Description:** raise error when api return error message. data in
    raw looks like:
      ```json
      {
        "1": 400,
    "2": "!ERROR:OBSOLETE_USER_PROFILE:coreGetUserStat - DROP non converted
    obsolete profile"
      }
      ```
     
    - **Issue:** #35 
    - **Dependencies:** None

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import httpx
 from httpx._utils import URLPattern
+import pytest
 from wt_profile_tool.main import WTProfileTool
 
 
@@ -34,6 +35,15 @@ def test_get_profile_by_userid(test_tool: WTProfileTool):
     data = test_tool.get_profile_by_userid("5363987")
     assert data.base_info.user_id == "5363987"
     assert data.base_info.nick == "OnTheRocks"
+
+
+def test_get_profile_by_userid_2(test_tool: WTProfileTool):
+    with pytest.raises(ValueError) as excinfo:
+        test_tool.get_profile_by_userid("9107392")
+    assert (
+        "WarThunder API return 400, message: !ERROR:OBSOLETE_USER_PROFILE:coreGetUserStat - DROP non converted obsolete profile"
+        == str(excinfo.value)
+    )
 
 
 def test_get_profile_by_nick(test_tool: WTProfileTool):
