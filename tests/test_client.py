14351400b625cf10bdd476b61ecd50a65953afc5
axiangcoding <axiangcoding@gmail.com>
2024-02-02 09:56:44 +0800

    wip

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import httpx
 from wt_profile_tool import WTPTClient
 
 
@@ -6,3 +7,20 @@ def test_init():
     assert client is not None
     assert isinstance(client, WTPTClient)
     assert client._WTPTClient__random_ua is False
+
+
+def test_init_2():
+    client = WTPTClient(random_ua=True)
+    assert client is not None
+    assert isinstance(client, WTPTClient)
+    assert client._WTPTClient__random_ua is True
+
+
+def test_init_3():
+    client = WTPTClient(
+        http_client=httpx.Client(timeout=httpx.Timeout(33.0)),
+    )
+    assert client is not None
+    assert isinstance(client, WTPTClient)
+    assert client._WTPTClient__random_ua is False
+    assert client._WTPTClient__http_client.timeout == httpx.Timeout(33.0)
