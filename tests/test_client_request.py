d658682417a8e1709b0c13a2d7e4786655de813e
axiangcoding <axiangcoding@gmail.com>
2024-02-08 11:08:22 +0800

    update

--- tests/test_client_request.py
+++ tests/test_client_request.py
@@ -2,12 +2,14 @@ import pytest
 from wt_profile_tool import WTPTClient
 
 
+@pytest.mark.scheduled
 def test_get_player_profile_by_userid(test_client: WTPTClient):
     data = test_client.get_player_profile_by_userid("5363987")
     assert data.base_info.user_id == "5363987"
     assert data.base_info.nick == "OnTheRocks"
 
 
+@pytest.mark.scheduled
 def test_get_player_profile_by_userid_2(test_client: WTPTClient):
     with pytest.raises(ValueError) as excinfo:
         test_client.get_player_profile_by_userid("9107392")
@@ -17,12 +19,14 @@ def test_get_player_profile_by_userid_2(test_client: WTPTClient):
     )
 
 
+@pytest.mark.scheduled
 def test_get_player_profile_by_nickname(test_client: WTPTClient):
     data = test_client.get_player_profile_by_nickname("OnTheRocks")
     assert data.base_info.user_id == "5363987"
     assert data.base_info.nick == "OnTheRocks"
 
 
+@pytest.mark.scheduled
 def test_get_player_userid_by_prefix_nick(test_client: WTPTClient):
     data = test_client.get_player_userid_by_prefix_nick("OnTheRocks")
     assert data.get_userid_by_nickname("OnTheRocks") == "5363987"
